7eb00ef5b7653145d8190e73d53936bd3edb3438
Abu Raihan <raihanahmed01973@gmail.com>
2023-02-01 18:19:53 +0600

    navbar responsive done, with toogle

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,26 @@
-import React from "react";
-import { ArrowLeftOnRectangleIcon } from "@heroicons/react/24/solid";
+import { Bars2Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 // import ParticlesBackground from "../Particles/ParticlesBackground";
 const Header = () => {
+  const [closeNav, setCloseNav] = useState(false);
+  const handleCloseNav = () => {
+    setCloseNav(!closeNav);
+    let navItems = document.getElementById("navItems");
+    if (!closeNav) {
+      navItems.style.display = "block";
+    } else {
+      navItems.style.display = "none";
+    }
+  };
   return (
     <div className="header-wrapper">
       <div className="header">
         <h2>
           <Link to="/">Raihan</Link>
         </h2>
-        <div className="nav-item">
+        <div className="nav-item" id="navItems">
           <Link to="/">Home</Link>
           <Link to="/project">Project</Link>
           <Link to="/contact">Contact</Link>
@@ -20,7 +30,15 @@ const Header = () => {
             </div>
           </button>
         </div>
+
         {/* <ParticlesBackground></ParticlesBackground> */}
+      </div>
+      <div className="navIcon">
+        {closeNav ? (
+          <XMarkIcon className="xmarkIcon" onClick={handleCloseNav}></XMarkIcon>
+        ) : (
+          <Bars2Icon className="barsIcon" onClick={handleCloseNav}></Bars2Icon>
+        )}
       </div>
     </div>
   );

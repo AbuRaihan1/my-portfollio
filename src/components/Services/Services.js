fcedf34d1d939df51d83e06773c1e9471ce218e2
Abu Raihan <raihanahmed01973@gmail.com>
2023-02-02 15:35:43 +0600

    services page done

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { Col, Container, Row, Tab, Tabs } from "react-bootstrap";
 import SectionTitle from "../SectionTItle/SectionTitle";
-import { CodeBracketIcon, PencilSquareIcon } from "@heroicons/react/24/solid";
+import { CodeBracketIcon, PencilSquareIcon, ArrowPathIcon } from "@heroicons/react/24/solid";
+import { FaHtml5, FaCss3Alt,FaMobileAlt,} from "react-icons/fa";
+import {BiSupport} from "react-icons/bi";
+import {FiSettings} from "react-icons/fi";
+
 import "./Services.css";
 const Services = () => {
   return (
@@ -9,7 +13,7 @@ const Services = () => {
       <SectionTitle title=" My Services"></SectionTitle>
       <Container>
         <Row>
-          <Tabs defaultActiveKey="tab-1" variant="tabs">
+          <Tabs defaultActiveKey="tab-1" variant="tabs" className="">
             <Tab eventKey="tab-1" title="Services">
               <Row>
                 <Col lg="4" md="6">
@@ -50,14 +54,15 @@ const Services = () => {
                     </p>
                   </div>
                 </Col>
+                
               </Row>
             </Tab>
             <Tab eventKey="tab-2" title="Features">
               <Row>
                 <Col lg="4" md="6">
                   <div className="inner-service-content">
-                    <CodeBracketIcon className="services-inner-icon"></CodeBracketIcon>
-                    <h2>Web Design</h2>
+                    <FaHtml5 className="services-inner-icon"></FaHtml5>
+                    <h2>Validate Html5</h2>
                     <p>
                       Conversion from PSD/XD/Figma to Pixel Perfect
                       Design,Device Responsive, jQuery Plugin Effects,Clean
@@ -69,8 +74,8 @@ const Services = () => {
 
                 <Col lg="4" md="6">
                   <div className="inner-service-content">
-                    <PencilSquareIcon className="services-inner-icon"></PencilSquareIcon>
-                    <h2>Graphic Design</h2>
+                    <FaCss3Alt className="services-inner-icon"></FaCss3Alt>
+                    <h2>Css3 Animation</h2>
                     <p>
                       Conversion from PSD/XD/Figma to Pixel Perfect
                       Design,Device Responsive, jQuery Plugin Effects,Clean
@@ -82,8 +87,8 @@ const Services = () => {
 
                 <Col lg="4" md="6">
                   <div className="inner-service-content">
-                    <CodeBracketIcon className="services-inner-icon"></CodeBracketIcon>
-                    <h2>Web Development</h2>
+                    <FaMobileAlt className="services-inner-icon"></FaMobileAlt>
+                    <h2>Fully Responsive</h2>
                     <p>
                       Conversion from PSD/XD/Figma to Pixel Perfect
                       Design,Device Responsive, jQuery Plugin Effects,Clean
@@ -98,8 +103,8 @@ const Services = () => {
               <Row>
                 <Col lg="4" md="6">
                   <div className="inner-service-content">
-                    <CodeBracketIcon className="services-inner-icon"></CodeBracketIcon>
-                    <h2>Web Design</h2>
+                    <ArrowPathIcon className="services-inner-icon"></ArrowPathIcon>
+                    <h2>Unlimited Revision</h2>
                     <p>
                       Conversion from PSD/XD/Figma to Pixel Perfect
                       Design,Device Responsive, jQuery Plugin Effects,Clean
@@ -111,8 +116,8 @@ const Services = () => {
 
                 <Col lg="4" md="6">
                   <div className="inner-service-content">
-                    <PencilSquareIcon className="services-inner-icon"></PencilSquareIcon>
-                    <h2>Graphic Design</h2>
+                    <BiSupport className="services-inner-icon"></BiSupport>
+                    <h2>Life Time Support</h2>
                     <p>
                       Conversion from PSD/XD/Figma to Pixel Perfect
                       Design,Device Responsive, jQuery Plugin Effects,Clean
@@ -124,8 +129,8 @@ const Services = () => {
 
                 <Col lg="4" md="6">
                   <div className="inner-service-content">
-                    <CodeBracketIcon className="services-inner-icon"></CodeBracketIcon>
-                    <h2>Web Development</h2>
+                    <FiSettings className="services-inner-icon"></FiSettings>
+                    <h2>Technical Support</h2>
                     <p>
                       Conversion from PSD/XD/Figma to Pixel Perfect
                       Design,Device Responsive, jQuery Plugin Effects,Clean

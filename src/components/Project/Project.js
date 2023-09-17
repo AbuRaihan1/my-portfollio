048342f0f461a65d7459776404f009957b3dd09f
Abu Raihan <raihanahmed01973@gmail.com>
2023-09-17 22:36:25 +0600

    updated projects

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -6,74 +6,56 @@ import "./Project.css";
 import courier from "./ProjectImg/courier.png";
 import darkDashboard from "./ProjectImg/darkDashboard.png";
 import shaajplastic from "./ProjectImg/shaajplastic.png";
+import vbdGazipur from "../../../src/images/vbdGazipur.png";
 const Project = () => {
+  const projects = [
+    {
+      img: vbdGazipur,
+      link: "https://vbd-gazipur.netlify.app/",
+      title: "VBD Gazipur",
+      text: "It's a fully dynamic project using react js and tailwind css. for awesome slider used swiper js.",
+    },
+
+    {
+      img: courier,
+      link: "https://aburaihan1.github.io/final-courier-/",
+      title: "Courier Service",
+      text: "it was a client project, courier site. i had work on it as a front-end designer.",
+    },
+    {
+      img: shaajplastic,
+      link: "https://aburaihan1.github.io/shaaj-plastic-industries-project-2/",
+      title: "Shaaj Plastic",
+      text: "it was a client project, courier site. i had work on it as a front-end designer.",
+    },
+    {
+      img: darkDashboard,
+      link: "https://aburaihan1.github.io/my-dashboard-dark/dark_theme.html",
+      title: "Graph",
+      text: "it was a client project, courier site. i had work on it as a front-end designer.",
+    },
+  ];
   return (
     <div className="project">
       <SectionTitle title="Projects i have done"></SectionTitle>
       <Container>
         <Row>
-          <Col md="6" lg="4">
-            <div className="projects-wrapper">
-              <Link
-                to="https://aburaihan1.github.io/final-courier-/"
-                target="_blank"
-              >
-                <div className="projects-item-wrapper">
-                  <img src={courier} alt="" />
-                  <div className="projects-wrapper-text">
-                    <h2>Courier Service</h2>
-                    <p>
-                      it was a client project, courier site. i had work on it as
-                      a front-end designer.
-                    </p>
-                    <button>Visit website</button>
+          {projects.map((project, idx) => (
+            <Col key={idx} xs={12} sm={6} md={4} lg={4}>
+              <div className="projects-wrapper">
+                <Link to={project.link} target="_blank">
+                  <div className="projects-item-wrapper">
+                    <img src={project.img} alt="" />
+                    <div className="projects-wrapper-text">
+                      <h2>{project.title}</h2>
+                      <p>{project.text}</p>
+                      <button>Visit website</button>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            </div>
-          </Col>
-
-          <Col md="6" lg="4">
-            <div className="projects-wrapper">
-              <Link
-                to="https://aburaihan1.github.io/shaaj-plastic-industries-project-2/"
-                target="_blank"
-              >
-                <div className="projects-item-wrapper">
-                  <img src={shaajplastic} alt="" />
-                  <div className="projects-wrapper-text">
-                    <h2>Shaaj Plastic</h2>
-                    <p>
-                      it was a client project, plasstic industires site. i had
-                      work on it as a front-end designer.
-                    </p>
-                    <button>Visit website</button>
-                  </div>
-                </div>
-              </Link>
-            </div>
-          </Col>
-
-          <Col md="6" lg="4">
-            <div className="projects-wrapper">
-              <Link
-                to="https://aburaihan1.github.io/my-dashboard-dark/dark_theme.html"
-                target="_blank"
-              >
-                <div className="projects-item-wrapper">
-                  <img src={darkDashboard} alt="" />
-                  <div className="projects-wrapper-text">
-                    <h2>Admin Dashboard</h2>
-                    <p>
-                      it was a client project, Admin Dashboard. i had work on it
-                      as a front-end development.
-                    </p>
-                    <button>Visit website</button>
-                  </div>
-                </div>
-              </Link>
-            </div>
-          </Col>
+                </Link>
+              </div>
+            </Col>
+          ))}
         </Row>
       </Container>
     </div>
